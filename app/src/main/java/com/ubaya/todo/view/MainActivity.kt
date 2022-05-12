fc9a65afd4b97e01323c0750a2bbb5d082939929
gilang <gilangkusuma90@gmail.com>
2022-05-12 10:04:10 +0700

    init

---
+++ app/src/main/java/com/ubaya/todo/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ubaya.todo.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ubaya.todo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
